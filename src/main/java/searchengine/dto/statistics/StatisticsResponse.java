121530901c8eb9ea43a5096dd3e3d951fd33a532
Dimas409 <82093956+Dimas409@users.noreply.github.com>
2024-09-08 17:43:37 +0300

    .

--- src/main/java/searchengine/dto/statistics/StatisticsResponse.java
+++ src/main/java/searchengine/dto/statistics/StatisticsResponse.java
@@ -1,7 +1,5 @@
 package searchengine.dto.statistics;
 
-import lombok.Data;
-
 public record StatisticsResponse(boolean result, StatisticsData statistics) {
     public StatisticsResponse(StatisticsData statistics) {
         this(true, statistics);
